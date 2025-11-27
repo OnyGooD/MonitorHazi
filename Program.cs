8bf161a5708b4498fe28f4ff8ffb69118f1ce0a3
GooDy <113987346+OnyGooD@users.noreply.github.com>
2025-11-27 11:01:45 +0100

    start

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace monitorok
+{
+    internal class Program
+    {
+        class Monitorok
+        {
+            public string Manufacturer { get; set; }
+            public string Model { get; set; }
+            public string Size { get; set; }
+            public string LCDType { get; set; }
+            public string Resolution { get; set; }
+            public string Range { get; set; }
+            public string Interface { get; set; }
+            public string LFC { get; set; }
+            public string Freesync { get; set; }
+            public string HDR { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
